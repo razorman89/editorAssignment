1ee58002924181bc099901e747cc9911654ba5b0
razorman89 <derekoc@campus.ie>
2012-12-04 19:57:40 +0000

    Updated this class to produce correct .gift syntax for multiplr choice questions with one correct answer

--- MoodleEditor/src/editor1/GiftFormatter.java
+++ MoodleEditor/src/editor1/GiftFormatter.java
@@ -2,6 +2,7 @@ package editor1;
 
 import java.util.ArrayList;
 
+import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 
@@ -48,6 +49,35 @@ public class GiftFormatter {
 		return multiAdvQuestion;
 	}
 	
+	public String buildMultiChoiceBas(String qTitle, String qBody, ArrayList<JTextField> answerFields, ArrayList<JTextField> feedbackFields, ArrayList<JRadioButton> radioSelectors) {
+		qTitle = stringConverter(qTitle);
+		qBody = stringConverter(qBody);
+		
+		for (int jj = 0; jj < answerFields.size(); jj++) {
+			String answerHolder = stringConverter(answerFields.get(jj).getText());
+			answerFields.get(jj).setText(answerHolder);
+			
+			String feedbackHolder = stringConverter(feedbackFields.get(jj).getText());
+			feedbackFields.get(jj).setText(feedbackHolder);
+			
+		}
+		
+		String multiBasQuestion = "";
+		multiBasQuestion = ("::" + qTitle + "::" + qBody + "{");
+		
+		for (int ii = 0; ii < answerFields.size(); ii++) {
+			if(radioSelectors.get(ii).isSelected()){
+				multiBasQuestion += ("\n=" + answerFields.get(ii).getText() + "\n#" + feedbackFields.get(ii).getText());
+			}else{
+				multiBasQuestion += ("\n~" + answerFields.get(ii).getText() + "\n#" + feedbackFields.get(ii).getText());
+			}
+		}
+		
+		multiBasQuestion += ("\n}");
+		
+		return multiBasQuestion;
+	}
+	
 	private String stringConverter(String string){
 		
 		System.out.println("BEFORE REPLACE: " + string);
@@ -63,7 +93,6 @@ public class GiftFormatter {
 		
 		return string;
 		
-	}
-	
+	}	
 
 }
