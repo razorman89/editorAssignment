a5a0d5c96130794e075d2478e2a7f5532da620c1
razorman89 <derekoc@campus.ie>
2012-12-06 01:14:49 +0000

    New Gui for Numerical questions added updates applied to other files

--- MoodleEditor/src/editor1/NumericalPane.java
+++ MoodleEditor/src/editor1/NumericalPane.java
@@ -1,5 +1,6 @@
 package editor1;
 
+import java.awt.Color;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,8 +27,8 @@ public class NumericalPane extends JPanel {
 	private ArrayList<JTextField> answerFields = new ArrayList<JTextField>();
 	private ArrayList<JTextField> feedbackFields = new ArrayList<JTextField>();
 	private ArrayList<JSpinner> answerSpinners = new ArrayList<JSpinner>();
-	private String[] options = {"-- Select Type --", "One Answer With Range", "Muliple Correct", "Within Range"};
-	private int rowCount = 1;
+	private String[] options = {"-- Select Type --", "Answer (+/-) Set Range", "Answer Within Set Range", "Muliple Answers With Partial Credit"};
+	private int rowCount = 0;
 	private int index = 0;
 //	private SpinnerNumberModel rangeSpinnerModel = new SpinnerNumberModel(0, 0, 1000.0, 0.001);
 //	private SpinnerNumberModel percentageSpinnerModel = new SpinnerNumberModel(0, 0, 100, 1);
@@ -46,6 +47,7 @@ public class NumericalPane extends JPanel {
 		
 		final JComboBox questionTypes = new JComboBox(options);
 		add(questionTypes, "cell 1 0,grow");
+		questionTypes.setSelectedIndex(0);
 		
 		JLabel lblQuestionTitle = new JLabel("Question Title:");
 		add(lblQuestionTitle, "cell 0 1,alignx right,aligny top");
@@ -72,6 +74,7 @@ public class NumericalPane extends JPanel {
 		add(scrollPane_3, "cell 1 3 4 1,grow");
 		
 		final JPanel answersPanel = new JPanel();
+		answersPanel.setVisible(false); //##
 		scrollPane_3.setViewportView(answersPanel);
 		answersPanel.setLayout(new MigLayout("", "[][130.00,grow][][130.00,grow][][68.00]", "[][][]"));
 		
@@ -106,64 +109,145 @@ public class NumericalPane extends JPanel {
 		answersPanel.add(txtFeedback_1, "flowy,cell 3 1,growx");
 		feedbackFields.add(txtFeedback_1);
 		
-		JSpinner ansSpinner = new JSpinner();
+		final JSpinner ansSpinner = new JSpinner();
+		ansSpinner.setEnabled(false); //##
 		answersPanel.add(ansSpinner, "flowy,cell 5 1,growx");
 		answerSpinners.add(ansSpinner);
 		
-		JTextField ansField2 = new JTextField();
-		ansField2.setToolTipText("Answer");
-		answersPanel.add(ansField2, "flowy,cell 1 2,growx");
-		answerFields.add(ansField2);
+//		JTextField ansField2 = new JTextField();
+//		ansField2.setToolTipText("Answer");
+//		answersPanel.add(ansField2, "flowy,cell 1 2,growx");
+//		answerFields.add(ansField2);
+//		
+//		JTextField txtFeedback_2 = new JTextField();
+//		txtFeedback_2.setToolTipText("Feedback");
+//		answersPanel.add(txtFeedback_2, "flowy,cell 3 2,growx");
+//		feedbackFields.add(txtFeedback_2);
+//		
+//		JSpinner ansSpinner1 = new JSpinner();
+//		answersPanel.add(ansSpinner1, "flowy,cell 5 2,growx");
+//		answerSpinners.add(ansSpinner1);
 		
-		JTextField txtFeedback_2 = new JTextField();
-		txtFeedback_2.setToolTipText("Feedback");
-		answersPanel.add(txtFeedback_2, "flowy,cell 3 2,growx");
-		feedbackFields.add(txtFeedback_2);
+		final JPanel rangePanel = new JPanel();
+		rangePanel.setVisible(false); //##
+		rangePanel.setBorder(new TitledBorder(null, "Range Options", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		add(rangePanel, "cell 1 4 2 2,grow");
+		rangePanel.setLayout(new MigLayout("", "[][100,grow][][][100,grow]", "[]"));
 		
-		JSpinner ansSpinner1 = new JSpinner();
-		answersPanel.add(ansSpinner1, "flowy,cell 5 2,growx");
-		answerSpinners.add(ansSpinner1);
+		final JLabel lblRangeStart = new JLabel("Range Start:");
+		rangePanel.add(lblRangeStart, "cell 0 0,alignx right");
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Range Options", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		add(panel, "cell 1 4 2 2,grow");
-		panel.setLayout(new MigLayout("", "[][100,grow][][][100,grow]", "[]"));
-		
-		JLabel lblRangeStart = new JLabel("Range Start:");
-		panel.add(lblRangeStart, "cell 0 0,alignx right");
-		
-		JTextField rangeStart = new JTextField();
-		panel.add(rangeStart, "cell 1 0,growx");
+		final JTextField rangeStart = new JTextField();
+		rangePanel.add(rangeStart, "cell 1 0,growx");
 		
 		JSeparator separator_4 = new JSeparator();
-		panel.add(separator_4, "cell 2 0,grow");
+		rangePanel.add(separator_4, "cell 2 0,grow");
 		separator_4.setOrientation(SwingConstants.VERTICAL);
 		
 		JLabel lblRangeEnd = new JLabel("Range End:");
-		panel.add(lblRangeEnd, "cell 3 0");
+		rangePanel.add(lblRangeEnd, "cell 3 0");
 		
-		JTextField rangeEnd = new JTextField();
-		panel.add(rangeEnd, "cell 4 0,growx");
+		final JTextField rangeEnd = new JTextField();
+		rangePanel.add(rangeEnd, "cell 4 0,growx");
 		
-		JButton btnNewAnswer = new JButton("Add Answer");		
+		final JButton btnNewAnswer = new JButton("Add Answer");	
+		btnNewAnswer.setEnabled(false); //##		
+		add(btnNewAnswer, "cell 3 4,growx");
+
+		final JButton btnClearText = new JButton("Clear Question Text");
+		btnClearText.setEnabled(false); //##
+		add(btnClearText, "cell 4 4,growx,aligny top");
+		
+		final JButton btnRemoveAnswer = new JButton("Remove Answer");
+		btnRemoveAnswer.setEnabled(false); //##
+		add(btnRemoveAnswer, "cell 3 5,growx,aligny top");
+		
+		final JButton btnCreateGifty = new JButton("Create Gift Code");
+		btnCreateGifty.setEnabled(false); //##
+		add(btnCreateGifty, "cell 4 5,growx,aligny top");
+		
+		JSeparator separator = new JSeparator();
+		add(separator, "cell 1 6 4 1,grow");
+		
+		JLabel lblGeneratedQuestions = new JLabel("Questions:");
+		add(lblGeneratedQuestions, "cell 0 7,alignx right,aligny top");
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		add(scrollPane_2, "cell 1 7 4 1,grow");
+		
+		final JTextArea questionsList = new JTextArea();
+		questionsList.setEditable(false);
+		questionsList.setBackground(SystemColor.control);
+		scrollPane_2.setViewportView(questionsList);
+		
+		JButton btnClearQuestionsList = new JButton("Clear Questions List");
+		add(btnClearQuestionsList, "cell 4 8,growx");
+		
+		JButton btnAddQuestions = new JButton("Add Questions to File");
+		add(btnAddQuestions, "cell 4 9,growx");
+
 		
 		questionTypes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				switch (questionTypes.getSelectedIndex()) {
 				case 0:
-					
+					autoClear(answersPanel, questionTitle, questionBody, rangeStart, rangeEnd);
+					answersPanel.setVisible(false);
+					rangePanel.setVisible(false);
+					btnNewAnswer.setEnabled(false);
+					btnRemoveAnswer.setEnabled(false);
+					btnClearText.setEnabled(false);
+					btnCreateGifty.setEnabled(false);
+					ansSpinner.setEnabled(false);
+					lblRangeStart.setText("Range Start:");
+					rangeEnd.setEditable(true);
+					rangeEnd.setBackground(Color.WHITE);
+	
 					break;
 					
 				case 1:
+					autoClear(answersPanel, questionTitle, questionBody, rangeStart, rangeEnd);
+					answersPanel.setVisible(true);
+					ansSpinner.setEnabled(false);
+					rangePanel.setVisible(true);
+					lblRangeStart.setText("Range (+/-) :");
+					rangeEnd.setEditable(false);
+					rangeEnd.setBackground(Color.LIGHT_GRAY);
+					btnNewAnswer.setEnabled(false);
+					btnRemoveAnswer.setEnabled(false);
+					btnClearText.setEnabled(true);
+					btnCreateGifty.setEnabled(true);
 					
 					break;
 					
 				case 2:
+					autoClear(answersPanel, questionTitle, questionBody, rangeStart, rangeEnd);
+					answersPanel.setVisible(true);
+					ansSpinner.setEnabled(false);
+					rangePanel.setVisible(true);
+					lblRangeStart.setText("Range Start:");
+					rangeEnd.setEditable(true);
+					rangeEnd.setBackground(Color.WHITE);
+					btnNewAnswer.setEnabled(false);
+					btnRemoveAnswer.setEnabled(false);
+					btnClearText.setEnabled(true);
+					btnCreateGifty.setEnabled(true);
 					
 					break;
 					
 				case 3:
-					
+					autoClear(answersPanel, questionTitle, questionBody, rangeStart, rangeEnd);
+					answersPanel.setVisible(true);
+					ansSpinner.setEnabled(true);
+					rangePanel.setVisible(true);
+					btnNewAnswer.setEnabled(true);
+					btnRemoveAnswer.setEnabled(true);
+					btnClearText.setEnabled(true);
+					btnCreateGifty.setEnabled(true);
+					lblRangeStart.setText("Range Start:");
+					rangeEnd.setEditable(true);
+					rangeEnd.setBackground(Color.WHITE);
+
 					break;
 
 				default:
@@ -198,64 +282,39 @@ public class NumericalPane extends JPanel {
 				answersPanel.updateUI();
 			}
 		});
-		add(btnNewAnswer, "cell 3 4,growx");
-
-		JButton btnClearText = new JButton("Clear Question Text");
-		add(btnClearText, "cell 4 4,growx,aligny top");
+		
+		
 		
 		btnClearText.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				clearInput(questionTitle, questionBody);
+				clearInput(questionTitle, questionBody, rangeStart, rangeEnd);
 			}
 		});
 				
-				JButton btnRemoveAnswer = new JButton("Remove Answer");
-				btnRemoveAnswer.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						if(rowCount > 1){
-							answersPanel.remove(answerFields.get(rowCount));
-							answersPanel.remove(feedbackFields.get(rowCount));
-							answersPanel.remove(answerSpinners.get(rowCount));
-							answerFields.remove(rowCount);
-							feedbackFields.remove(rowCount);
-							answerSpinners.remove(rowCount);
-							answersPanel.updateUI();
-							rowCount--;					
-						}
-					}
-				});
-				add(btnRemoveAnswer, "cell 3 5,growx,aligny top");
 		
-				JButton btnCreateGifty = new JButton("Create Gift Code");
-				add(btnCreateGifty, "cell 4 5,growx,aligny top");
+		btnRemoveAnswer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(rowCount > 0){
+					answersPanel.remove(answerFields.get(rowCount));
+					answersPanel.remove(feedbackFields.get(rowCount));
+					answersPanel.remove(answerSpinners.get(rowCount));
+					answerFields.remove(rowCount);
+					feedbackFields.remove(rowCount);
+					answerSpinners.remove(rowCount);
+					answersPanel.updateUI();
+					rowCount--;					
+				}
+			}
+		});
 				
-				btnCreateGifty.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						index++;
+				
+		btnCreateGifty.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				index++;
 //						questionsMultiAdvGiftyList.append("//Question " + index + "\n" + localFormatter.buildMultiChoiceAdv(questionTitle.getText(), questionBody.getText(), answerFields, feedbackFields, answerSpinners) + "\n\n");
-						clearInput(questionTitle, questionBody);
-					}
-				});
-		
-		JSeparator separator = new JSeparator();
-		add(separator, "cell 1 6 4 1,grow");
-		
-		JLabel lblGeneratedQuestions = new JLabel("Questions:");
-		add(lblGeneratedQuestions, "cell 0 7,alignx right,aligny top");
-		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		add(scrollPane_2, "cell 1 7 4 1,grow");
-		
-		final JTextArea questionsList = new JTextArea();
-		questionsList.setEditable(false);
-		questionsList.setBackground(SystemColor.control);
-		scrollPane_2.setViewportView(questionsList);
-		
-		JButton btnClearQuestionsList = new JButton("Clear Questions List");
-		add(btnClearQuestionsList, "cell 4 8,growx");
-		
-		JButton btnAddQuestions = new JButton("Add Questions to File");
-		add(btnAddQuestions, "cell 4 9,growx");
+				clearInput(questionTitle, questionBody, rangeStart, rangeEnd);
+			}
+		});
 		
 		btnClearQuestionsList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -266,15 +325,41 @@ public class NumericalPane extends JPanel {
 
 	}
 	
-	private void clearInput(final JTextArea questionTitle, final JTextArea questionBody) {
+	private void clearInput(final JTextArea questionTitle, final JTextArea questionBody, JTextField rangeStart, JTextField rangeEnd) {
 		questionTitle.setText("");
 		questionBody.setText("");
+		rangeStart.setText("");
+		rangeEnd.setText("");
+		
 		for (int ii = 0; ii < answerFields.size(); ii++) {
 			answerFields.get(ii).setText("");
 			feedbackFields.get(ii).setText("");
 			answerSpinners.get(ii).setValue(0);
 			
 		}
+	}
+	
+	private void autoClear(final JPanel answersPanel, JTextArea questionTitle, JTextArea questionBody, JTextField rangeStart, JTextField rangeEnd) {
+//		System.out.println("\n-B-\nANSWERS: " + answerFields.size() + "\nFEEDBACK: " + feedbackFields.size() + "\nSPINNERS: " + answerSpinners.size() + "\n-B-");
+		if(rowCount > 0){
+//			System.out.println("COUNT = " + rowCount);
+			int temp = rowCount;
+			for (int ii = temp; ii > 0 ; ii--) {
+				System.out.println("DELETING ROW: " + ii);
+				answersPanel.remove(answerFields.get(ii));
+				answersPanel.remove(feedbackFields.get(ii));
+				answersPanel.remove(answerSpinners.get(ii));
+				answerFields.remove(ii);
+				feedbackFields.remove(ii);
+				answerSpinners.remove(ii);
+				answersPanel.updateUI();	
+				rowCount--;
+			}
+		}else if(rowCount <= 0){
+//			System.out.println("SKIPPED");
+		}
+//		clearInput(questionTitle, questionBody, rangeStart, rangeEnd);
+//		System.out.println("-A-\nANSWERS: " + answerFields.size() + "\nFEEDBACK: " + feedbackFields.size() + "\nSPINNERS: " + answerSpinners.size() + "\n-A-\n");
 	}
 
 }
